b0f73b6645da2c3a2f2eb735e1145ef1ea8eb761
bhasamritasarmah <bhasamritasarmah@gmail.com>
2023-06-17 21:37:30 +0530

    Made a few minor changes to correct the requirements.txt file.

--- resume_parser_python_api/resume_parser.py
+++ resume_parser_python_api/resume_parser.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/home", methods = ["GET"])
 def Home():
-    return "Welcome to Bhasamrita Sarmah's webpage."
+    return "Welcome to Bhasamrita Sarmah's page."
 
 if __name__ == "__main__":
-    app.run(host = "localhost", port = 3000)
+    app.run()
